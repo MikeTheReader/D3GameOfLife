96f8c71665f2b01ef6af80eac0741666e4f8d134
MikeTheReader <mlundin617@gmail.com>
2015-11-07 10:25:09 -0800

    Another change test updates -- this time to htmlpreview.github.io page

--- js/main.js
+++ js/main.js
@@ -59,11 +59,11 @@ function onload() {
     createShape(acorn, cells, 50, 50, '#AA0000');
     //createShape(smallExploder, cells, 10, 10, '#330033');
     createShape(acorn, cells, 50, 10, '#AA3300');
-    //createShape(acorn, cells, 50, 15, '#33AA00');
-    //createShape(acorn, cells, 50, 20, '#333300');
+    createShape(acorn, cells, 50, 15, '#33AA00');
+    createShape(acorn, cells, 50, 20, '#333300');
     //createShape(exploder, cells, 85, 30, '#00AA00');
     createShape(glider, cells, 25, 25, '#0000AA');
-    createShape(gliderGun, cells, 0, 0, '#AAAA00');
+    //createShape(gliderGun, cells, 0, 0, '#AAAA00');
     //createShape(exploder, cells, 80, 60, '#00AAAA');
     
 	animate(svg, cells);
